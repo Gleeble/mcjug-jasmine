d1660a9b2cdf94b05d9ed15bb3d7d696bcd99a89
Adam Swift <aswift@5amsolutions.com>
2014-01-13 23:53:45 -0500

    Refactored Test JS a bit and added initial spec definitions

---
+++ example-spec/spec/charCountSpec.js
@@ -0,0 +1,45 @@
+describe("The tests for the charCount.js example file", function() {
+
+    describe("Tests for the example Character Count function", function() {
+
+        it("counts strings of single characters", function() {
+            expect(true).toBe(false);
+        });
+
+        it("counts strings with different characters", function() {
+            expect(true).toBe(false);
+        });
+
+        it("counts strings with numbers and letters", function() {
+            expect(true).toBe(false);
+        });
+
+        it("counts the non-alphanumeric characters of strings", function() {
+            expect(true).toBe(false);
+        });
+
+        it("Treats all alphabetic characters as uppercase", function() {
+            expect(true).toBe(false);
+        });
+        
+    });
+
+    describe("Tests for displaying the character counts", function() {
+        it("Displays the characters in sorted order", function() {
+            expect(true).toBe(false);
+        });
+        
+        it("outputs all items as a ul", function() {
+            expect(true).toBe(false);
+        });
+        
+        it("writes the resultant html to the element with ID 'output'", function() {
+            expect(true).toBe(false);
+        });
+        
+        it("displays each character with the count in the same format", function() {
+            expect(true).toBe(false);
+        });
+    });
+});
+
